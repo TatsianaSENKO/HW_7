ba88351b46da5f7c09918be48022f401bddd0616
Tatsiana <134013154+TatsianaSENKO@users.noreply.github.com>
2023-11-16 10:00:01 +0300

    Add onLater

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let Alarm = function(t, s, c, h, m) {
     this.color = c || 'green';
     this.hourSet = h || null;
     this.minSet = m || null;
+    this.onLater;
 
     this.onOff = function() {
         if (this.hourSet !== null && this.minSet !== null) this.status = true;
@@ -16,14 +17,22 @@ let Alarm = function(t, s, c, h, m) {
         let dateNow = new Date();
         // console.log(dateNow);
         if (this.hourSet == dateNow.getHours() && this.minSet == dateNow.getMinutes()) {
-            alert (this.signal + '\n'+ this.hourSet + ':' + this.minSet);
+            this.onLater = confirm (this.signal + '\n'+ this.hourSet + ':' + this.minSet + '\n'+ 'Напомнить через 5 минут?');
         };
+    }
+
+    this.later = function() {
+        let dateLater = new Date();
+        if (this.onLater == true && dateLater.getMinutes() == this.minSet + 1) {
+            alert (this.signal + '\n'+ this.hourSet + ':' + dateLater.getMinutes());
+        }
     }
 }
 
-let alarm1 = new Alarm(1, 'trrr-trrr-trrr', 'yellow', 9, 14);
+let alarm1 = new Alarm(1, 'trrr-trrr-trrr', 'yellow', 9, 55);
 
 alarm1.onOff();
 alarm1.on();
+alarm1.later();
 
 console.log(alarm1);
